1e37d8b56cac7ef565cad78f62557d642fb2f257
Darius Teng <dariusyteng@gmail.com>
2024-08-07 19:17:00 -0400

    Pull down updates

--- frontend/src/screens/GalleryScreenTest.jsx
+++ frontend/src/screens/GalleryScreenTest.jsx
@@ -1,60 +1,39 @@
-import React, { useState, useEffect } from 'react';
+import React, { useState } from 'react';
 import {
   List,
   ListItem,
   ListItemText,
   Typography,
-  Button,
   Container,
-  CircularProgress,
+  Pagination,
 } from '@mui/material';
-import Pagination from '@mui/material/Pagination';
-const recipesData = require('../assets/data/recipes');
+
+const recipes = require('../assets/data/recipes'); // Ensure this path correctly points to your recipes data file
 
 const GalleryScreen = () => {
-  const [loading, setLoading] = useState(false);
   const [currentPage, setCurrentPage] = useState(1);
   const recipesPerPage = 10;
 
   const indexOfLastRecipe = currentPage * recipesPerPage;
   const indexOfFirstRecipe = indexOfLastRecipe - recipesPerPage;
-  const currentRecipes = recipesData.slice(
-    indexOfFirstRecipe,
-    indexOfLastRecipe
-  );
+  const currentRecipes = recipes.slice(indexOfFirstRecipe, indexOfLastRecipe);
+
+  const totalPages = Math.ceil(recipes.length / recipesPerPage);
 
   const paginate = (event, value) => {
     setCurrentPage(value);
   };
 
-  useEffect(() => {
-    setLoading(true);
-    setTimeout(() => {
-      // fecth
-      setLoading(false);
-    }, 500);
-  }, []);
-
-  const totalPages = Math.ceil(recipesData.length / recipesPerPage);
-
   return (
-    <Container maxWidth='sm' style={{ marginTop: '20px' }}>
-      <Typography variant='h4' style={{ marginBottom: '20px' }} color='primary'>
-        Dish Gallery
-      </Typography>
-
-      {loading ? (
-        <CircularProgress />
-      ) : (
-        <List>
-          {currentRecipes.map((recipe) => (
-            <ListItem key={recipe._id} divider>
-              <ListItemText primary={recipe.name} />
-            </ListItem>
-          ))}
-        </List>
-      )}
-
+    <Container maxWidth='sm'>
+      <Typography variant='h4'>Dish Gallery</Typography>
+      <List>
+        {currentRecipes.map((recipe) => (
+          <ListItem key={recipe._id} divider>
+            <ListItemText primary={recipe.name} />
+          </ListItem>
+        ))}
+      </List>
       <Pagination
         count={totalPages}
         page={currentPage}
@@ -62,7 +41,6 @@ const GalleryScreen = () => {
         color='primary'
         showFirstButton
         showLastButton
-        style={{ marginTop: '20px', display: 'flex', justifyContent: 'center' }}
       />
     </Container>
   );
